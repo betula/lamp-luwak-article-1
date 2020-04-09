853cc7c2e9d5fcbe18e3fa1f20afddc4d752a941
betula <mail@betula.co>
2020-04-09 17:46:52 +0700

    [todos] Fix app

--- todos/src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { NewTodo } from './NewTodo';
-
-export const Header = () => (
-  <header className="header">
-    <h1>todos</h1>
-    <NewTodo />
-  </header>
-);
